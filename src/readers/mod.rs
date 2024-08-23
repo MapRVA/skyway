f736b6c4cf0ca984e5e069d554334e14417a6554
Jacob Hall <email@jacobhall.net>
2024-08-23 11:15:30 -0400

    remove XML reader module (for now)

--- src/readers/mod.rs
+++ src/readers/mod.rs
@@ -9,9 +9,6 @@ use pbf::read_pbf;
 mod json;
 use json::read_json;
 
-mod xml;
-use xml::read_xml;
-
 pub fn read_file<S: Read + Send>(sender: Sender<elements::Element>, metadata_sender: Sender<elements::Metadata>, from: &str, mut source: S) {
     match from {
         "json" => {
@@ -25,16 +22,6 @@ pub fn read_file<S: Read + Send>(sender: Sender<elements::Element>, metadata_sen
             read_json(sender, metadata_sender, source_str);
         },
         "pbf" => read_pbf(sender, metadata_sender, source),
-        "xml" => {
-            let mut buffer = String::new();
-            let source_str = match source.read_to_string(&mut buffer) {
-                Ok(_) => buffer.as_str(),
-                Err(e) => {
-                    panic!("Error reading input: {e:?}");
-                },
-            };
-            read_xml(sender, metadata_sender, source_str);
-        },
         _ => panic!("Filetype not supported!")
     }
 }
