a2cff6a0c833c35bc0cbc5ebad061567e45221ed
Jacob Hall <email@jacobhall.net>
2024-09-03 09:21:17 -0400

    improve o5m bit-packing functions, add some tests for them

--- src/writers/o5m.rs
+++ src/writers/o5m.rs
@@ -24,45 +24,6 @@ fn convert_tag(key: &str, value: &str) -> Vec<u8> {
     output
 }
 
-fn convert_index(index: usize) -> Vec<u8> {
-    unimplemented!()
-}
-
-struct TagTable {
-    cached_tags: Vec<(String, String, Vec<u8>)>,
-}
-
-impl TagTable {
-    fn get_bytes(mut self, key: String, value: String) -> Vec<u8> {
-        // tags with key+value longer than 250 chars get to
-        // skip the TagTable altogether!
-        if key.len() + value.len() > 250 {
-            convert_tag(&key, &value);
-        }
-
-        // determine index of key, value in TagTable
-        // tag_position will be Option<usize>
-        let tag_position = self
-            .cached_tags
-            .iter()
-            .position(|x| x.0 == key && x.1 == value);
-
-        // if key, value were found in TagTable, return
-        // index converted to o5m byte sequence. Otherwise,
-        // insert the new tag into the TagTable then return
-        // its byte sequence
-        match tag_position {
-            Some(v) => convert_index(v),
-            None => {
-                let output = convert_tag(&key, &value);
-                self.cached_tags.insert(0, (key, value, output.clone()));
-                // TODO: cull self.cached_tags if it's length is > 15000
-                output
-            }
-        }
-    }
-}
-
 fn convert_f64(input: f64) -> Vec<u8> {
     unimplemented!()
 }
@@ -104,21 +65,60 @@ fn convert_number(bytes: &[u8]) -> Vec<u8> {
 
 // convert a signed 64-bit integer (i64) into the bit-packed
 // specification for o5m, returned as a Vec of bytes (u8)
-fn convert_id(input: i64) -> Vec<u8> {
+fn convert_i64_as_unsigned(input: i64) -> Vec<u8> {
     convert_number(&input.to_be_bytes())
 }
 
 // convert a user id (i32) and username (String) into the
 // bit-packed specification for o5m, returned as a Vec of bytes (u8)
 fn convert_user(uid: i32, username: String) -> Vec<u8> {
-    // let mut output = Vec::new();
-    // output.push(0x00);
-    // output.extend(username.as_bytes());
-    // output.push(0x00);
-    // output.extend(convert_number(&uid.to_be_bytes()));
-    // output.push(0x00);
-    // output
-    unimplemented!()
+    let mut output = Vec::new();
+    output.push(0x00);
+    output.extend(convert_number(&uid.to_be_bytes()));
+    output.push(0x00);
+    output.extend(username.as_bytes());
+    output.push(0x00);
+    output
+}
+
+fn convert_index(index: usize) -> Vec<u8> {
+    convert_number(&index.to_be_bytes())
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct StringPair(Vec<u8>);
+
+struct StringTable {
+    cached_tags: Vec<StringPair>,
+}
+
+impl StringTable {
+    fn hit_cache(&mut self, bytes: Vec<u8>) -> Vec<u8> {
+        let str_pair = StringPair(bytes);
+
+        // determine index of key, value in TagTable
+        // tag_position will be Option<usize>
+        let tag_position = self.cached_tags.iter().position(|b| b == &str_pair);
+
+        // if key, value were found in TagTable, return
+        // index converted to o5m byte sequence. Otherwise,
+        // insert the new tag into the TagTable then return
+        // its byte sequence
+        match tag_position {
+            // increment by 1 to accomodate zero-indexed vector
+            Some(v) => convert_index(v + 1),
+            None => {
+                self.cached_tags.insert(0, str_pair.clone());
+                self.cached_tags.truncate(15000);
+                str_pair.0
+            }
+        }
+    }
+    fn new() -> Self {
+        StringTable {
+            cached_tags: Vec::new(),
+        }
+    }
 }
 
 pub fn write_o5m<D: Write>(receiver: Receiver<Element>, metadata: Metadata, mut dest: D) {
@@ -162,24 +162,48 @@ mod tests {
     fn test_convert_id() {
         let input1: i64 = 5;
         let expected1 = vec![0x05];
-        assert_eq!(convert_id(input1), expected1);
+        assert_eq!(convert_i64_as_unsigned(input1), expected1);
 
         let input2: i64 = 127;
         let expected2 = vec![0x7f];
-        assert_eq!(convert_id(input2), expected2);
+        assert_eq!(convert_i64_as_unsigned(input2), expected2);
 
         let input3: i64 = 323;
         let expected3 = vec![0xc3, 0x02];
-        assert_eq!(convert_id(input3), expected3);
+        assert_eq!(convert_i64_as_unsigned(input3), expected3);
 
         let input4: i64 = 16384;
         let expected4 = vec![0x80, 0x80, 0x01];
-        assert_eq!(convert_id(input4), expected4);
+        assert_eq!(convert_i64_as_unsigned(input4), expected4);
     }
     #[test]
     fn test_convert_user() {
         let input1: (i32, String) = (1020, String::from("John"));
         let expected1 = vec![0x00, 0xfc, 0x07, 0x00, 0x4a, 0x6f, 0x68, 0x6e, 0x00];
         assert_eq!(convert_user(input1.0, input1.1), expected1);
+    }
+
+    #[test]
+    fn test_string_table() {
+        let mut string_table = StringTable::new();
+
+        let vec1 = vec![
+            0x00, 0x6f, 0x6e, 0x65, 0x77, 0x61, 0x79, 0x00, 0x79, 0x65, 0x73, 0x00,
+        ];
+        assert_eq!(string_table.hit_cache(vec1.clone()), vec1);
+
+        let vec2 = vec![0x00, 0x61, 0x74, 0x6d, 0x00, 0x6e, 0x6f, 0x00];
+        assert_eq!(string_table.hit_cache(vec2.clone()), vec2);
+
+        assert_eq!(string_table.hit_cache(vec1.clone()), vec![0x02]);
+
+        let vec3 = vec![0x00, 0xfc, 0x07, 0x00, 0x4a, 0x6f, 0x68, 0x6e, 0x00];
+        assert_eq!(string_table.hit_cache(vec3.clone()), vec3);
+
+        assert_eq!(string_table.hit_cache(vec2), vec![0x02]);
+
+        assert_eq!(string_table.hit_cache(vec1), vec![0x03]);
+
+        assert_eq!(string_table.hit_cache(vec3), vec![0x01]);
     }
 }
