d96623c51505682ad720fe207b235946a32190b5
Jacob Hall <email@jacobhall.net>
2024-09-13 13:48:29 -0400

    rewrite JSON writer to be much faster

--- src/writers/json.rs
+++ src/writers/json.rs
@@ -1,126 +1,218 @@
-use serde::{Serialize, Serializer};
-use serde_json::to_writer;
-use std::collections::HashMap;
-use std::io::Write;
-use std::sync::mpsc::Receiver;
+use json::stringify;
+use lexical;
+use rayon::prelude::*;
+use std::fmt::{Error, Write};
+use std::sync::mpsc::{channel, Receiver};
 
-use crate::elements::{Element, ElementType, Member, Metadata, SimpleElementType};
+use crate::elements::{Element, ElementType, Metadata, SimpleElementType};
+use crate::threadpools::WRITER_THREAD_POOL;
 
-fn serialize_simple_element_type<S>(
-    value: &Option<SimpleElementType>,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
+// wrapper struct that implements std::fmt::Write for any type
+// that implements std::io::Write
+struct ToFmtWrite<T>(pub T);
+
+impl<T> Write for ToFmtWrite<T>
 where
-    S: Serializer,
+    T: std::io::Write,
 {
-    match value {
-        Some(SimpleElementType::Node) => serializer.serialize_str("node"),
-        Some(SimpleElementType::Way) => serializer.serialize_str("way"),
-        Some(SimpleElementType::Relation) => serializer.serialize_str("relation"),
-        None => serializer.serialize_none(),
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        self.0.write_all(s.as_bytes()).map_err(|_| std::fmt::Error)
     }
 }
 
-#[derive(Serialize)]
-#[serde(remote = "Member")]
-pub struct MemberDef {
-    #[serde(rename = "type", serialize_with = "serialize_simple_element_type")]
-    pub t: Option<SimpleElementType>,
-    #[serde(rename = "ref")]
-    pub id: i64,
-    pub role: Option<String>,
+fn create_header(metadata: Metadata) -> String {
+    let mut header = String::from("{");
+
+    if let Some(c) = metadata.copyright {
+        header.push_str("\"copyright\":");
+        header.push_str(&stringify(c));
+        header.push(',');
+    }
+
+    // TODO: attribution as well as copyright?
+
+    if let Some(l) = metadata.license {
+        header.push_str("\"license\":");
+        header.push_str(&stringify(l));
+        header.push(',');
+    }
+
+    // TODO: add skyway details to this?
+    if let Some(g) = metadata.generator {
+        header.push_str("\"generator\":");
+        header.push_str(&stringify(g));
+        header.push(',');
+    }
+
+    header.push_str("\"version\":\"0.6\",\"elements\":[");
+
+    header
 }
 
-#[derive(Serialize)]
-#[serde(remote = "ElementType", tag = "type", rename_all = "lowercase")]
-pub enum ElementTypeDef {
-    Node {
-        lat: f64,
-        lon: f64,
-    },
-    Way {
-        nodes: Vec<i64>,
-    },
-    Relation {
-        #[serde(serialize_with = "serialize_member_vec")]
-        members: Vec<Member>,
-    },
-}
+fn append_serialized_element(base: &mut String, element: Element) {
+    // start this node
+    base.push_str("{\"type\":");
 
-fn serialize_member_vec<S: Serializer>(v: &[Member], serializer: S) -> Result<S::Ok, S::Error> {
-    #[derive(Serialize)]
-    struct Wrapper<'a>(#[serde(with = "MemberDef")] &'a Member);
+    // take care of type-specific attributes
+    // (dict order doesn't matter in JSON)
+    match element.element_type {
+        ElementType::Node { lat, lon } => {
+            base.push_str("\"node\"");
 
-    v.iter()
-        .map(Wrapper)
-        .collect::<Vec<_>>()
-        .serialize(serializer)
-}
+            base.push_str(",\"lat\":");
+            base.push_str(&lexical::to_string(lat));
 
-fn _skip_visibility(visibility: &Option<bool>) -> bool {
-    visibility.unwrap_or(true)
-}
+            base.push_str(",\"lon\":");
+            base.push_str(&lexical::to_string(lon));
+        }
+        ElementType::Way { nodes } => {
+            // finish "type": "way", then start nodes dict
+            base.push_str("\"way\",\"nodes\":[");
 
-#[derive(Serialize)]
-#[serde(remote = "Element")]
-pub struct ElementDef {
-    pub changeset: Option<i64>,
-    pub user: Option<String>,
-    pub version: Option<i32>,
-    pub uid: Option<i32>,
-    pub id: i64,
-    pub timestamp: Option<String>,
-    #[serde(skip_serializing_if = "_skip_visibility")]
-    pub visible: Option<bool>,
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
-    pub tags: HashMap<String, String>,
-    #[serde(flatten, with = "ElementTypeDef")]
-    pub element_type: ElementType,
-}
+            // fill in nodes dict
+            let mut first_node_appended = false;
+            for n in nodes {
+                if first_node_appended {
+                    base.push(',');
+                }
+                first_node_appended = true;
+                base.push_str(&lexical::to_string(n));
+            }
+            // close nodes dict
+            base.push(']');
+        }
+        ElementType::Relation { members } => {
+            // finish "type": "relation", then start members list
+            base.push_str("\"relation\",\"members\":[");
 
-#[derive(Serialize)]
-#[serde(remote = "Metadata")]
-pub struct MetadataDef {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub version: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub generator: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub copyright: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub license: Option<String>,
-}
+            // fill in members list
+            let mut first_node_appended = false;
+            for m in members {
+                if first_node_appended {
+                    base.push(',');
+                }
+                first_node_appended = true;
 
-#[derive(Serialize)]
-pub struct OsmDocument {
-    #[serde(flatten, with = "MetadataDef")]
-    pub metadata: Metadata,
-    #[serde(serialize_with = "serialize_element_vec")]
-    pub elements: Vec<Element>,
-}
+                base.push('{');
 
-fn serialize_element_vec<S: Serializer>(v: &[Element], serializer: S) -> Result<S::Ok, S::Error> {
-    #[derive(Serialize)]
-    struct Wrapper<'a>(#[serde(with = "ElementDef")] &'a Element);
+                base.push_str("\"ref\":");
+                base.push_str(&lexical::to_string(m.id));
 
-    v.iter()
-        .map(Wrapper)
-        .collect::<Vec<_>>()
-        .serialize(serializer)
-}
+                match m.t {
+                    Some(SimpleElementType::Node) => base.push_str(",\"type\":\"node\""),
+                    Some(SimpleElementType::Way) => base.push_str(",\"type\":\"way\""),
+                    Some(SimpleElementType::Relation) => base.push_str(",\"type\":\"relation\""),
+                    None => (),
+                }
 
-pub fn write_json<D: Write>(receiver: Receiver<Vec<Element>>, metadata: Metadata, dest: D) {
-    let received_elements: Vec<Element> = receiver.into_iter().flatten().collect();
+                base.push_str(",\"role\":");
+                match m.role {
+                    Some(r) => base.push_str(&stringify(r)),
+                    None => base.push_str("\"\""),
+                }
 
-    let osm_document = OsmDocument {
-        metadata,
-        elements: received_elements,
-    };
-
-    match to_writer(dest, &osm_document) {
-        Ok(_) => (),
-        Err(e) => {
-            panic!("JSON serialization error: {e:?}");
+                base.push('}');
+            }
+            base.push(']');
         }
     }
+
+    base.push_str(",\"id\":");
+    base.push_str(&lexical::to_string(element.id));
+
+    if let Some(c) = element.changeset {
+        base.push_str(",\"changeset\":");
+        base.push_str(&lexical::to_string(c));
+    }
+
+    if let Some(t) = element.timestamp {
+        base.push_str(",\"timestamp\":");
+        base.push_str(&stringify(t));
+    }
+
+    if let Some(u) = element.uid {
+        base.push_str(",\"uid\":");
+        base.push_str(&lexical::to_string(u));
+    }
+
+    if let Some(u) = element.user {
+        base.push_str(",\"user\":");
+        base.push_str(&stringify(u));
+    }
+
+    // add visible field only if it is false
+    if element.visible == Some(false) {
+        base.push_str(",\"visible\":false");
+    }
+
+    // append this element's tags to base
+    if !element.tags.is_empty() {
+        base.push_str(",\"tags\":{");
+        let mut first_tag_appended = false;
+        for (k, v) in element.tags {
+            if first_tag_appended {
+                base.push(',');
+            }
+            first_tag_appended = true;
+            base.push_str(&stringify(k));
+            base.push(':');
+            base.push_str(&stringify(v));
+        }
+        base.push('}');
+    }
+
+    // finish element
+    base.push('}');
+}
+
+fn serialize_chunk(chunk: Vec<Element>) -> Result<String, Error> {
+    let mut output = String::new();
+    let mut first_element_appended = false;
+    for element in chunk {
+        if first_element_appended {
+            output.push(',');
+        }
+        first_element_appended = true;
+        append_serialized_element(&mut output, element);
+    }
+    Ok(output)
+}
+
+pub fn write_json<D: std::io::Write>(
+    receiver: Receiver<Vec<Element>>,
+    metadata: Metadata,
+    dest: D,
+) {
+    let mut writer = ToFmtWrite(dest);
+
+    // TODO: append metadata to output
+
+    let (output_sender, output_reciever) = channel();
+    WRITER_THREAD_POOL.install(move || {
+        receiver
+            .into_iter()
+            .par_bridge()
+            .map(serialize_chunk)
+            .map(|result| result.expect("Failed to serialize chunk"))
+            .for_each(|s| match output_sender.clone().send(s) {
+                Ok(_) => (),
+                Err(e) => panic!("Error passing output chunk between threads: {e:?}"),
+            });
+    });
+
+    let header = create_header(metadata);
+
+    writer
+        .write_str(&header)
+        .expect("Couldn't write opening metadata to output.");
+
+    for output_string in output_reciever {
+        writer
+            .write_str(&output_string)
+            .expect("Failed to write to output");
+    }
+
+    writer
+        .write_str("]}")
+        .expect("Couldn't write final closing curly brace to output.");
 }
